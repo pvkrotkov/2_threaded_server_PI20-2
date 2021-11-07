c1c1e63ebdecb78654ad11c97e5cdda3a810528b
Anastasia Lim <72342708+almondloverr@users.noreply.github.com>
2021-11-07 18:13:39 +0300

    server.py updated

--- server.py
+++ server.py
@@ -1,20 +1,36 @@
 import socket
+import threading
+
+
+def client(conn, addr):
+    print('Запустился новый поток')
+    while True:
+        data = conn.recv(1024)
+        if not data:
+            break
+        print(data.decode())
+        conn.send(data)
+
 
 sock = socket.socket()
-sock.bind(('', 9090))
-sock.listen(0)
-conn, addr = sock.accept()
-print(addr)
-
-msg = ''
+port = 9090
 
 while True:
-	data = conn.recv(1024)
-	if not data:
-		break
-	msg += data.decode()
-	conn.send(data)
+    try:
+        sock.bind(('', port))
+        break
+    except:
+        print(f'Порт занят {port}')
+        port += 1
 
-print(msg)
+print (f'Сервер подключился к порту: {port}')
+sock.listen(3)
 
-conn.close()
+while True:
+    conn, addr = sock.accept()
+    print(f'Клиент {addr} подключился к серверу')
+    p = threading.Thread(target = client, args = (conn, addr))
+    p.start()
+
+sock.close()
+print('Сервер завершил работу')
