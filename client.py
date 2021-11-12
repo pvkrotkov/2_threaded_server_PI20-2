04680ca74a6442e4dc3b9602c4ec67be6bec3246
AMITGAN <45467874+AMITGAN@users.noreply.github.com>
2021-11-12 17:42:33 +0300

    Update client.py

--- client.py
+++ client.py
@@ -1,16 +1,12 @@
 import socket
-from time import sleep
 
 sock = socket.socket()
-sock.setblocking(1)
-sock.connect(('10.38.165.12', 9090))
-
-#msg = input()
-msg = "Hi!"
-sock.send(msg.encode())
-
-data = sock.recv(1024)
-
+sock.connect(('127.0.0.1', 3000))
+while True:
+    n = input()
+    sock.send(n.encode())
+    pata = sock.recv(1024)
+    print (pata.decode())
+    if n == "exit":
+        break
 sock.close()
-
-print(data.decode())
