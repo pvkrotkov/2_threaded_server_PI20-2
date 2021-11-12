64adaa67ee27168e54aad4220f3ffb69dab4b738
AMITGAN <45467874+AMITGAN@users.noreply.github.com>
2021-11-12 17:43:40 +0300

    Update server.py

--- server.py
+++ server.py
@@ -1,20 +1,13 @@
 import socket
 
 sock = socket.socket()
-sock.bind(('', 9090))
-sock.listen(0)
-conn, addr = sock.accept()
-print(addr)
-
-msg = ''
-
+sock.bind(('127.0.0.1', 3000))
+sock.listen(1)
+conn, aadr = sock.accept()
 while True:
-	data = conn.recv(1024)
-	if not data:
-		break
-	msg += data.decode()
-	conn.send(data)
-
-print(msg)
+    data = conn.recv(1024)
+    conn.send(data.upper())
+    if data.decode() == "exit":
+        break
 
 conn.close()
