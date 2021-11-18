c9d73dc2f07b6e9e4a07442ca7135e3d71e60864
Lam1nO <adelka200215@gmail.com>
2021-11-18 17:09:08 +0300

    scanner added

--- scanner.py
+++ scanner.py
@@ -1,15 +1,34 @@
 import socket
-from threading import Thread
+import threading
 
-N = 2**16 - 1
+isOpen = [0]*3000
+checked = [0]*3000
 
-for port in range(1,100):
+def open (port):
     sock = socket.socket()
+    sock.settimeout(0.5)
     try:
-        print(port)
-        sock.connect(('127.0.0.1', port))
-        print("Порт", i, "открыт")
+        sock.connect(('www.yandex.ru', port))
     except:
-        continue
-    finally:
         sock.close()
+        checked[port] = 1
+    else:
+        isOpen[port] = checked[port] = 1
+        sock.close()
+
+
+t = [threading.Thread(target=open, args=[i]) for i in range(1, 3000)]
+[t1.start() for t1 in t]
+
+a = 0
+while a  < 2999:
+    a = sum(checked)
+    print (str(a) + " of 2999")
+    
+[t1.join() for t1 in t]
+
+for i in range (0, 3000):
+    if isOpen[i] == 1:
+        print ("Port", i + 1, "is opened")
+    else:
+        print ("Port", i + 1, "is not opened")
